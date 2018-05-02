375d0e69f2a3e76f2deb7c888924b97f507ab1bf
Cabrokimer <akeymer@gmail.com>
2018-05-02 22:36:50 +0200

    aa

---
+++ chembl.R
@@ -0,0 +1,27 @@
+pacman::p_load_gh("rajarshi/chemblr/package")
+pacman::p_load(tidyverse, RCurl, rJava, RJSONIO)
+
+
+# Drugbank_converted.csv is a query of `data/fulldatabase.xml`
+# The query is 'data/basex/to_csv.xq"
+drugbank <-
+    read_delim("data/drugbank_converted.csv", delim = ";",
+               col_names = c("name", "inchi", "receptor", "gene", "action", "known"))
+
+# Selected drug is a manual list of psychiatric drugs"
+selected_drugs <- 
+    read_csv("data/clean_data/drugs.csv")
+
+drugs <- 
+    drugbank %>%
+    filter(name %in% selected_drugs$name) %>%
+    mutate(receptor = str_to_lower(receptor))
+
+
+
+
+
+####
+
+chemblr::get.compounds(drugs$inchi[1], type="stdinchi")
+
--- data/xquery/to_csv.xq
+++ data/xquery/to_csv.xq
@@ -6,11 +6,11 @@ for $action in $target/actions
 for $gene in $target/polypeptide
 for $atrs in $drug/atc-codes/atc-code
 for $smile in $drug/calculated-properties/property
-where $smile[kind="SMILES"]
+where $inchi[kind="InChIKey"]
   return
     concat(
           $qt, $drug/name/text(), $qt, $sep,
-          $qt, $smile/value, $qt, $sep,
+          $qt, $inchi/value, $qt, $sep,
           $qt, $drug/half-life/text(), $qt, $sep,
           $qt, $atrs/@code/string(), $qt, $sep,
           $qt, $target/name, $qt, $sep,
