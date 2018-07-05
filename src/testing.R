1511a9f90c4b6023191c968bb8b553fe290e386b
Cabrokimer <akeymer@gmail.com>
2018-07-05 22:35:03 +0200

    sddd

--- src/testing.R
+++ src/testing.R
@@ -1,59 +1,93 @@
-pacman::p_load(rvest, tidyverse, ggthemes, png, RCurl, grid, magick, rsvg)
+pacman::p_load(rvest, tidyverse)
+
+drugs <- read_csv("data/drugs.csv")
+
+ 
 
 
-
-drug <- "DB00334"
-df <- 
-    url(paste0("https://www.drugbank.ca/drugs/", drug)) %>%
-    read_html() %>%
-    html_nodes(css ='.bond-list')
-
-
-
-tt <- 
-    map_df(1:length(xml_contents(df[[1]])), function(x) get_target(df, x=1, y= x))
-
-
-
-get_target <- function(node, x = 1, y = 1){
-    name <- 
-        xml_child(node[[x]], y) %>%
-        html_nodes('strong') %>%
-        html_text()
-
-    vals <- 
-        xml_child(node[[x]], y) %>%
-        html_nodes('dd') %>%
-        html_text()
-
-    cols <-
-        xml_child(node[[x]], y) %>%
-        html_nodes('dt') %>%
-        html_text()
-
-    binding <- 
-        try(
-        xml_child(node[[x]], y) %>%
-            html_nodes('.table.table-sm.table-responsive') %>%
-            html_table() %>%
-            .[[1]] %>%
-            select(col = 1, value = 2) %>%
-            group_by(col) %>%
-            summarise(min = min(value),
-                      max = max(value),
-                      med = median(value)) %>%
-            gather(key, value, -col) %>%
-            unite(key, col, key) %>%
-            arrange(key) %>%
-            spread(key, value)
-        )
-
-    df <- 
-        tibble(cols, vals, Name = name) %>%
-        spread(cols, vals)
-
-    if(class(binding)[1] != "try-error"){
-        bind_cols(df, binding)} else {
-            df
-    }
+get_selection <- function(query){
+    drugs <- read_csv("data/drugs.csv")
+    match <- agrep(pattern = query, x = drugs$name, value = T)[1]
+    selection <- 
+        drugs %>%
+        filter(name == match)
+    return(selection)
 }
+get_nodes <- function(drug){
+    df <- 
+        url(paste0("https://www.drugbank.ca/drugs/", pull(drug, drugbank_id))) %>%
+        read_html() %>%
+        html_nodes(css ='.bond-list')
+    return(df)
+}
+get_targets_df <- function(df, section) {
+    get_target <- function(node, x, y){
+        drug <- 
+            xml_child(node[[x]], y) %>%
+            html_nodes('strong') %>%
+            html_text()
+        
+        vals <- 
+            xml_child(node[[x]], y) %>%
+            html_nodes('dd') %>%
+            html_text()
+        
+        cols <-
+            xml_child(node[[x]], y) %>%
+            html_nodes('dt') %>%
+            html_text()
+        
+        binding <- 
+            try(
+                xml_child(node[[x]], y) %>%
+                    html_nodes('.table.table-sm.table-responsive') %>%
+                    html_table() %>%
+                    .[[1]] %>%
+                    select(col = 1, value = 2) %>%
+                    mutate(value = str_remove(xx$value, pattern = '>'),
+                           value = as.numeric(value)) %>%
+                    group_by(col) %>%
+                    summarise(min = min(value),
+                              max = max(value),
+                              med = median(value)) %>%
+                    gather(key, value, -col) %>%
+                    unite(key, col, key) %>%
+                    arrange(key) %>%
+                    spread(key, value),
+                silent = T)
+        
+        df <- 
+            tibble(cols, vals, Name = drug) %>%
+            spread(cols, vals)
+        
+        if(class(binding)[1] != "try-error"){
+            bind_cols(df, binding)} else {
+                df
+            }
+    }
+    map_df(1:length(xml_contents(df[[section]])), function(x)
+        get_target(df, x = section, y = x)) %>%
+        separate(Name, into = c("on", "Name"), extra = "merge") %>%
+        select(-on) %>%
+        mutate(
+            Actions = str_replace(.$Actions, pattern = '([:upper:][:lower:]+)([:upper:][:lower:]+)',
+                                  '\\1 - \\2')
+        )
+}
+
+do_all <- function(query, section = 1){
+    sel <- get_selection(query)
+    sel %>%
+        get_nodes() %>%
+        get_targets_df(section) %>%
+        mutate(Drug = sel$name,
+               drugbank_id = sel$drugbank_id)
+}
+
+
+test <- 
+do_all("Nicotine")
+
+rr <- 
+bind_rows(
+map(kk, do_all)
