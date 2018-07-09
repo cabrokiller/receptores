39c9ac4160eed9358f73e0fe99f6dac756ae7abd
cabrokiller <cabrokiller@gmail.com>
2018-07-09 15:19:30 +0200

    ddddd

--- src/testing.R
+++ src/testing.R
@@ -1,67 +1,53 @@
 pacman::p_load(rvest, tidyverse, ggthemes, png, RCurl, grid, magick, rsvg, RColorBrewer)
 
 
-data_target <- read_csv("data/drugbank_target_parse.csv")
-data_enzyme <- read_csv("data/drugbank_enzyme_parse.csv")
-do_activity_plot <- function(molecule = "Haloperidol", data = data_enzyme){
-    # get specific molecule df
-    df <- 
-        data %>%
-        filter(Drug == molecule)
-    # get number of `General Function` to plot
-    n <-
-        length(unique(df$`General Function`))
-    
-    # get molecule image
-    mol_img <-
-        df %>%
-        pull(drugbank_id) %>%
-        unique() %>%
-        paste0('https://www.drugbank.ca/structures/', ., '/image.svg') %>%
-        image_read_svg() %>%
-        image_colorize(opacity = 100, color = '#839496')
-    
-    # plot
-    df %>%
-        select(Name, Actions, `General Function`) %>%
-        mutate(Actions = str_replace_all(.$Actions, pattern = c('([:upper:][:lower:]+)' = "\\1 -"))) %>%
-        separate(Actions, into = c("action1","action2", "action3"), extra = "drop", fill = "left") %>%
-        mutate(action3 = ifelse(action3 == "", NA, action3)) %>%
-        gather(key,Actions, -c(Name, `General Function`), na.rm = T) %>%
-        select(-key) %>%
-        # plot
-        ggplot(aes(x=`Name`, y=Actions, fill=str_remove(`General Function`, ',.+'))) +
-        annotation_custom(rasterGrob(mol_img)) +
-        geom_bin2d(alpha = .6) +
-        # geom_point(size=10, shape=15, alpha = .8) +
-        coord_flip() +
-        scale_fill_manual(values = colorRampPalette(brewer.pal(n = 12, name = 'Paired'))(n)) +
-        labs(title = str_to_title(molecule), x = "", y="", fill = "Function") +
-        theme_minimal()
-}
+data_target <- read_csv("../data/drugbank_target_parse.csv")
+data_enzyme <- read_csv("../data/drugbank_enzyme_parse.csv")
 
-molecule = "Haloperidol"
+
+
+
+molecule <- "Haloperidol"
+data <- data_enzyme
+
+
+
+
 df <- 
-data_enzyme %>%
-    filter(Drug == "Haloperidol")
+    data %>%
+    filter(Drug == molecule)
+# get number of `General Function` to plot
 n <-
     length(unique(df$`General Function`))
 
+# get molecule image
+mol_img <-
+    df %>%
+    pull(drugbank_id) %>%
+    unique() %>%
+    paste0('https://www.drugbank.ca/structures/', ., '/image.svg') %>%
+    image_read_svg() %>%
+    image_colorize(opacity = 100, color = '#839496')
+
+# plot
 df %>%
-    select(Name, Actions, `General Function`) %>%
+    select(Name, Actions, `General Function`, `Ki (nM)_min`, `Ki (nM)_max`, `Ki (nM)_med`) %>%
     mutate(Actions = str_replace_all(.$Actions, pattern = c('([:upper:][:lower:]+)' = "\\1 -"))) %>%
     separate(Actions, into = c("action1","action2", "action3"), extra = "drop", fill = "left") %>%
     mutate(action3 = ifelse(action3 == "", NA, action3)) %>%
-    gather(key,Actions, -c(Name, `General Function`), na.rm = T) %>%
+    gather(key,Actions, -c(Name, `General Function`, `Ki (nM)_min`, `Ki (nM)_max`, `Ki (nM)_med`), na.rm = T) %>%
     select(-key) %>%
+    mutate(Actions = str_to_title(Actions)) %>%
     # plot
-    ggplot(aes(x=`Name`, y=Actions, fill=str_remove(`General Function`, ',.+')))+
+    ggplot(aes(x=`Name`, color=Actions, y = `Ki (nM)_med`))+
     annotation_custom(rasterGrob(mol_img)) +
-    geom_point(size=15, shape=21, alpha = .8) +
+    geom_errorbar(aes(ymin = `Ki (nM)_min`, ymax = `Ki (nM)_max`), fatten = 10, 
+                  alpha = .8, width = .6, position = position_dodge(.6), size = 1.2)+
+    geom_point(aes(y = `Ki (nM)_med`), position = position_dodge(.6),
+               size = 6, alpha = .8, shape = 19) +
+    scale_color_manual(values = colorRampPalette(brewer.pal(12, 'Set3'))(n))+
+    labs(title = str_to_title(molecule), x = "", y="", color = "Function") +
+    ggplot2::scale_y_log10() +
     coord_flip() +
-    scale_fill_manual(values = colorRampPalette(solarized_pal()(8))(n)) +
-    labs(title = str_to_title(molecule), x = "", y="", fill = "Function") +
     theme_minimal()
-
-
     
