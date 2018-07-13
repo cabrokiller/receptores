92d3c8b7e4e014786b1da4bd6ee741ab98bfb470
cabrokiller <akeymer@gmail.com>
2018-07-13 16:10:54 +0200

    xxx

--- drugs/server.R
+++ drugs/server.R
@@ -11,43 +11,16 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    pacman::p_load(rvest, tidyverse, ggthemes, png, RCurl, grid, magick, rsvg, RColorBrewer)
+    source("../src/testing.R")
     output$drugPlot <- renderPlot({
         molecule <- input$select
         ifelse(input$radio == 1,
-                data <- read_csv("../data/drugbank_target_parse.csv"),
-                data <- read_csv("../data/drugbank_enzyme_parse.csv"))
-        df <- 
-            data %>%
-            filter(Drug == molecule)
-        # get number of `General Function` to plot
-        n <-
-            length(unique(df$`General Function`))
+                type <- "target",
+                type <- "enzyme")
         
-        # get molecule image
-        mol_img <-
-            df %>%
-            pull(drugbank_id) %>%
-            unique() %>%
-            paste0('https://www.drugbank.ca/structures/', ., '/image.svg') %>%
-            image_read_svg() %>%
-            image_colorize(opacity = 100, color = '#839496')
+        do_plot_3(molecule = molecule, type = type)
         
-        # plot
-        df %>%
-            select(Name, Actions, `General Function`) %>%
-            mutate(Actions = str_replace_all(.$Actions, pattern = c('([:upper:][:lower:]+)' = "\\1 -"))) %>%
-            separate(Actions, into = c("action1","action2", "action3"), extra = "drop", fill = "left") %>%
-            mutate(action3 = ifelse(action3 == "", NA, action3)) %>%
-            gather(key,Actions, -c(Name, `General Function`), na.rm = T) %>%
-            select(-key) %>%
-            # plot
-            ggplot(aes(y=`Name`, fill=Actions, x=Actions))+
-            annotation_custom(rasterGrob(mol_img)) +
-            geom_bin2d(alpha = .65) +
-            scale_fill_manual(values = colorRampPalette(solarized_pal()(8))(n))+
-            labs(title = str_to_title(molecule), x = "", y="", fill = "Function") +
-            theme_minimal()
+        
   })
   
 })
--- src/testing.R
+++ src/testing.R
@@ -1,8 +1,8 @@
 pacman::p_load(rvest, tidyverse, ggthemes, png, RCurl, grid, magick, rsvg, RColorBrewer, scales)
 
 
-data_target <- read_csv("data/drugbank_target_parse.csv")
-data_enzyme <- read_csv("data/drugbank_enzyme_parse.csv")
+data_target <- read_csv("../data/drugbank_target_parse.csv")
+data_enzyme <- read_csv("../data/drugbank_enzyme_parse.csv")
 
 
 do_plot_1 <- function(molecule = "Clozapine", type = "target"){
@@ -41,8 +41,6 @@ do_plot_1 <- function(molecule = "Clozapine", type = "target"){
         labs(title = str_to_title(molecule), x = "", y="", fill = "Function") +
         theme_minimal()
 }
-
-do_plot_1()
 
 do_plot_2 <- function(molecule = "Clozapine", type = "target"){
     data_full <- 
@@ -88,24 +86,47 @@ do_plot_2 <- function(molecule = "Clozapine", type = "target"){
         theme_minimal()
 }
 
-do_plot_2("Ziprasidone")
+do_plot_3 <- function(molecule = "Clozapine", type = "target"){
+    data_full <- 
+        data_target %>%
+        mutate(Type = "target") %>%
+        bind_rows(data_enzyme %>%
+                      mutate(Type = "enzyme"))
+    
+    for_plot <-   
+        data_full %>%
+        filter(Drug == molecule,
+               Type == type) %>%
+        mutate(Actions = str_replace_all(.$Actions, pattern = c('([:upper:][:lower:]+)' = "\\1 -"))) %>%
+        separate(Actions, into = c("action1","action2", "action3"), extra = "drop", fill = "left") %>%
+        mutate(action3 = ifelse(action3 == "", NA, action3)) %>%
+        gather(key,Actions, -c(Name, `Gene Name`:Type), na.rm = T) %>%
+        select(-key) %>%
+        mutate(Pot = ifelse(is.na(`Ki (nM)_med`), 0, -log10(`Ki (nM)_med`)))
+    
+    n_names <- 
+        for_plot %>%
+        tally() %>%
+        pull(n)
+    
+    mol_img <-
+        for_plot %>%
+        pull(drugbank_id) %>%
+        unique() %>%
+        paste0('https://www.drugbank.ca/structures/', ., '/image.svg') %>%
+        image_read_svg() %>%
+        image_colorize(opacity = 100, color = '#839496')
+    
+    
+    for_plot %>%
+        ggplot(aes(y = fct_reorder(`Name`, -log(`Ki (nM)_med`)),
+                   x = Actions)) +
+        annotation_custom(rasterGrob(mol_img)) +
+        geom_bin2d(aes(fill = log10(`Ki (nM)_med`)), alpha = .75) +
+        scale_fill_viridis_c(option = "C") +
+        labs(title = unique(for_plot$Drug), x = '', y = "Targets", shape = 'Potence') +
+        theme_minimal()
+    
 
+    }
 
-
-for_plot <-   
-    data_full %>%
-    filter(Drug == "Ziprasidone",
-           Type == 'target') %>%
-    mutate(Actions = str_replace_all(.$Actions, pattern = c('([:upper:][:lower:]+)' = "\\1 -"))) %>%
-    separate(Actions, into = c("action1","action2", "action3"), extra = "drop", fill = "left") %>%
-    mutate(action3 = ifelse(action3 == "", NA, action3)) %>%
-    gather(key,Actions, -c(Name, `Gene Name`:Type), na.rm = T) %>%
-    select(-key) %>%
-    mutate(Pot = ifelse(is.na(`Ki (nM)_med`), 0, log10(`Ki (nM)_med`)/`Ki (nM)_med`))
-
-zz <- 
-for_plot %>%
-    select(xx = `Ki (nM)_med`) %>%
-    mutate(xxx = log10(xx), 
-           xy = log10(xx)/xx) %>%
-    arrange(xx)
