9086c9910093cbebef45e87360ecc61d2f183d56
Cabrokimer <akeymer@gmail.com>
2018-12-12 23:07:29 +0100

    cool

--- drugs_2/ui.R
+++ drugs_2/ui.R
@@ -2,11 +2,13 @@ library(shiny)
 library(dplyr)
 library(readr)
 
-drugs <- 
-    read_csv("https://raw.githubusercontent.com/cabrokiller/receptores/master/data/drugs.csv")
+drugs <-
+    read_csv(
+        "https://raw.githubusercontent.com/cabrokiller/receptores/master/data/drugs.csv"
+    )
 
 
-get_fam <- function(df,family){
+get_fam <- function(df, family) {
     df %>%
         filter(fam %in% family) %>%
         arrange(name)
@@ -14,35 +16,49 @@ get_fam <- function(df,family){
 
 ad <- get_fam(drugs, c("Antidepressant", "Mood stabilizer"))
 ap <- get_fam(drugs, "Antipsychotic")
-ot <- get_fam(drugs, c("Other", "Stimulant", "Opioid", "Depressants"))
+ot <-
+    get_fam(drugs, c("Other", "Stimulant", "Opioid", "Depressants"))
 
 
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(theme = "bootstrap.css",
+shinyUI(fluidPage(
+    theme = "bootstrap.css",
     titlePanel("Perfiles farmacodinámicos"),
-    h5("Aplicación para visualizar de manera simple el perfil receptorial
-        de hasta 3 fármacos. Es posible seleccionar la familia de fármacos a desplegar en las listas"),
-    fluidRow(
-            column(2,
-                tabsetPanel(
-                    tabPanel("Antidepresivos/eutimizantes",
-                        checkboxGroupInput("drugs_1",
-                        h4("Antidepresivos / eutimizantes"),
-                        choices = as.list(ad$name))
-                        ),
-                    tabPanel("antipsi",
-                        checkboxGroupInput("drugs_2",
-                        h4("Antipsicóticos"),
-                        choices = as.list(ap$name),
-                        selected = c("Aripiprazole", "Olanzapine", "Sertindole"))
-                        ),
-                    tabPanel("other",
-                        checkboxGroupInput("drugs_3",
-                        h4("Otros"),
-                        choices = as.list(ot$name))
-                ))),
-            column(10,
-            plotOutput("drugPlot",
-                       height = '900px',
-                        width = "100%")))))
+    h5(
+        "Aplicación para visualizar de manera simple el perfil receptorial
+        de hasta 3 fármacos. Es posible seleccionar la familia de fármacos a desplegar en las listas"
+    ),
+    fluidRow(column(
+        2,
+        tabsetPanel(
+            tabPanel(
+                "Antipsicóticos",
+                checkboxGroupInput(
+                    "drugs_2",
+                    NULL,
+                    choices = as.list(ap$name),
+                    selected = c("Aripiprazole", "Olanzapine", "Sertindole")
+                )
+            ),
+            tabPanel(
+                "Antidepresivos/eutimizantes",
+                checkboxGroupInput("drugs_1",
+                                   NULL,
+                                   choices = as.list(ad$name))
+            ),
+            tabPanel(
+                "Otros",
+                checkboxGroupInput("drugs_3",
+                                   NULL,
+                                   choices = as.list(ot$name))
+            )
+        )
+    ),
+    column(
+        10,
+        plotOutput("drugPlot",
+                   height = '900px',
+                   width = "100%")
+    ))
+))
