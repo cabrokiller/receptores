a033fc313f2cd967a2436208e4ea24062b1fc019
cabrokiller <cabrokiller@gmail.com>
2020-11-14 19:02:21 +0100

    ok

--- src/testing.R
+++ src/testing.R
@@ -4,7 +4,7 @@ pacman::p_load(tidyverse)
 clean <- read_csv("data/clean.csv")
 
 molecule <-
-    c("Aripiprazole", "Lurasidone", "Cariprazine")
+    c("Aripiprazole", "Lurasidone", "Clozapine")
 
 my_family <- 
     c("DRD", "HTR")
@@ -25,17 +25,26 @@ my_symbols <-
     pull(symbol_2)
 
 
+### test for fa
+my_symbols <- 
+fontawesome(c("fa-caret-up", "fa-caret-down", "fa-caret-square-o-up", "fa-caret-square-o-down"))
 
 
-ggplot(for_plot) +
-geom_point(aes(x = drug_name, y = receptor)) +
-    
 
+
+
+
+ggplot(for_plot, aes(x = `Pharmacological action`, y = receptor, shape = symbol_2, color = log(Ki))) +
+geom_point(size = 
+               8) +
 scale_shape_manual(values = my_symbols) +
     scale_size(range = c(10,20)) +
     scale_color_viridis_c(direction = -1, na.value = "gray40", option = "B") +
-    facet_grid(family ~ `Pharmacological action`, scales = "free_y", space = "free_y") +
-    theme_minimal_grid()
+    facet_grid(family ~ drug_name, scales = "free_y", space = "free_y") +
+    ggthemes::theme_solarized_2(light = F) +
+    theme(text = element_text(family = "serif"))
+
+
 
 
 ######## 
